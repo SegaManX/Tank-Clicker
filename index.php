d8368620573a63af5dc2a39a974eacfb4210d680
SegaManX <karlis.egle@gmail.com>
2022-12-04 03:54:01 +0200

    added logins

--- index.php
+++ index.php
@@ -1,39 +1,77 @@
-<!DOCTYPE HTML>
+<?php
+include("config.php");
+session_start();
+
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+  // Username and password from form
+
+  $myusername = mysqli_real_escape_string($db,$_POST['username']);
+  $mypassword = mysqli_real_escape_string($db,$_POST['password']);
+  $result = mysqli_query($db, "SELECT admin FROM users WHERE username = '$myusername' and password = '$mypassword'");
+  $result = $result->fetch_array();
+  $admin = intval($result[0]);
+
+  if($admin)
+  {
+    $_SESSION['login_user'] = $myusername;
+    header("location: game.php");
+
+  }
+  else if(!$admin)
+  {
+    $_SESSION['login_user'] = $myusername;
+    header("location: copy.php");
+  }
+  else
+  {
+    $error = "Your Login Name or Password is invalid";
+  }
+  
+}
+?>
 <html>
 
 <head>
-  <title>0 $ - Tank Clicker</title>
+  <title>Sign In - Tank Clicker</title>
 
-  <link rel="stylesheet" href="main.css">
+  <style type = "text/css">
+    body {
+      font-family:Arial, Helvetica, sans-serif;
+      font-size:14px;
+    }
+    label {
+      font-weight:bold;
+      width:100px;
+      font-size:14px;
+    }
+    .box {
+      border:#666666 solid 1px;
+    }
+  </style>
 </head>
 
-<body>
-  <div class="sectionLeft">
-    <center>
-      <div class="scoreContainer unselectable">
-        <span id="score">0</span> $<br>
-        <span id="scorePS">0</span> $ per second
+<body bgcolor = "#FFFFFF">
+	
+      <div align = "center">
+         <div style = "width:300px; border: solid 1px #333333; " align = "left">
+            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
+				
+            <div style = "margin:30px">
+               
+               <form action = "" method = "post">
+                  <label>Username  :</label><input type = "text" name = "username" class = "box"/><br /><br />
+                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
+                  <input type = "submit" value = " Submit "/><br />
+               </form>
+               
+               <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php if (isset($error)){echo $error;}; ?></div>
+					
+            </div>
+				
+         </div>
+			
       </div>
-      <br>
-      <div class="clickerContainer unselectable">
-        <img src="images/tank.png" height="500px" width="600px" id="clicker" class="unselectable">
-				</div>
-			</center>
-			<div class=" sectionFooter">
-        <h5>Tank Clicker™</h5>
-        <button onclick=" saveGame()">Save Game</button>
-        <button onclick="resetGame()">Reset Game</button>
-      </div>
-      <div class="NextLvl">
-        <button onclick="NextLevel()">Next Level</button> <span id="scoreBot">0</span> / <span id="nextSc">0</span>
-      </div>
-  </div>
 
-  <div class="sectionRight">
-    <div id="upgradeContainer"></div>
-    <div id="shopContainer"></div>
-  </div>
-  <script src="main.js"></script>
-</body>
-
+   </body>
 </html>
